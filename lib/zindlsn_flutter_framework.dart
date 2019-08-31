feead2d24080756f7f0e5300849bde79d405d2bb
Stefan Zindl <st148777@stud.uni-stuttgart.de>
2019-08-31 02:42:04 +0200

    Adds info which classes to expose.

--- lib/zindlsn_flutter_framework.dart
+++ lib/zindlsn_flutter_framework.dart
@@ -1,5 +1,8 @@
 library zindlsn_flutter_framework;
 
+///
+/// all this classes will be published for the user of this package.
+///
 export 'package:zindlsn_flutter_framework/mwwm/view_model/base_view_model.dart';
 export 'package:zindlsn_flutter_framework/mwwm/view/base_view.dart';
 export 'package:zindlsn_flutter_framework/mwwm/registry/locator.dart';
