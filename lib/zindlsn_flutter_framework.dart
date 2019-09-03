7035a475137a760e990e0f824623e088f99e85a3
Stefan Zindl <st148777@stud.uni-stuttgart.de>
2019-09-04 00:12:38 +0200

    Adds global NavigationService.

--- lib/zindlsn_flutter_framework.dart
+++ lib/zindlsn_flutter_framework.dart
@@ -6,3 +6,4 @@ library zindlsn_flutter_framework;
 export 'package:zindlsn_flutter_framework/mwwm/view_model/base_view_model.dart';
 export 'package:zindlsn_flutter_framework/mwwm/view/base_view.dart';
 export 'package:zindlsn_flutter_framework/mwwm/registry/locator.dart';
+export 'package:zindlsn_flutter_framework/services/navigation_service.dart';
