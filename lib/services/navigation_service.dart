ec76b68bfea980886b6ecd65a40bd197c2a9d928
Stefan Zindl <st148777@stud.uni-stuttgart.de>
2019-09-06 19:44:25 +0200

    Adds optional arguments when navigate to other page.

--- lib/services/navigation_service.dart
+++ lib/services/navigation_service.dart
@@ -4,8 +4,8 @@ class NavigationService {
   final GlobalKey<NavigatorState> navigatorKey =
   new GlobalKey<NavigatorState>();
 
-  Future<dynamic> navigateTo(String routeName) {
-    return navigatorKey.currentState.pushNamed(routeName);
+  Future<dynamic> navigateTo(String routeName, {dynamic arguments}) {
+    return navigatorKey.currentState.pushNamed(routeName, arguments: arguments);
   }
 
   bool goBack() {
