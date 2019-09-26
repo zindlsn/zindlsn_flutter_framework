034eb22a23ebb29de4d1db4c6c8154bd87cf693c
zindlsn <photosketch@outlook.de>
2019-09-27 01:14:01 +0200

    Fix for disposing viewModels.

--- lib/mwwm/view_model/base_view_model.dart
+++ lib/mwwm/view_model/base_view_model.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-/**
- * Declares the states of the view model.
- */
+///
+/// Declares the states of the view model.
+///
 enum ViewState { Idle, Busy}
 
-/**
- * Each view model extends this baseviewmodel to have the base
- * functionality.
- */
+///
+/// Each view model extends this baseviewmodel to have the base
+/// functionality.
+///
 class BaseViewModel extends ChangeNotifier {
+  bool mounted = true;
   ViewState _state = ViewState.Idle;
 
   ViewState get state => _state;
 
   void setState(ViewState viewState) {
+    if(mounted){
     _state = viewState;
     notifyListeners();
+    }
+  }
+
+  @override
+  void dispose(){
+    mounted = false;
+    super.dispose();
   }
 }
