05f69b6d994a2833db16890cc0a4144b37f8c116
zindlsn <photosketch@outlook.de>
2019-09-27 12:23:06 +0200

    Flutter upgrade.

--- test/zindlsn_flutter_framework_test.dart
+++ test/zindlsn_flutter_framework_test.dart
@@ -1,7 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:zindlsn_flutter_framework/zindlsn_flutter_framework.dart';
-
 void main() {
 /**  test('adds one to input values', () {
     final calculator = Calculator();
