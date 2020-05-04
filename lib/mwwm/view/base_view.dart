ecc55cbc1e73a7f8ec12775e0131c924e04073ea
zindlsn <photosketch@outlook.de>
2020-05-04 20:11:47 +0200

    Update provider dependency

--- lib/mwwm/view/base_view.dart
+++ lib/mwwm/view/base_view.dart
@@ -33,7 +33,7 @@ class _BaseViewState<T extends ChangeNotifier> extends State<BaseView<T>> {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<T>(
-      builder: (context) => model,
+      create: (context) => model,
       child: Consumer<T>(builder: widget.builder),
     );
   }
